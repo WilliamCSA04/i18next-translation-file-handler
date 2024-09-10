0060a799a72acfc77cc9e5163dd5ed64d0f2271d
williamCS04 <williamcaetano14@gmail.com>
2024-09-10 14:52:41 -0300

    :art: Moved functions to utils

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,8 @@
 mod file;
 mod utils;
 use clap::{self, Command};
-use log::{self, error, info, warn};
-use std::{collections::HashSet, error::Error};
-
-use walkdir::WalkDir;
-
-fn get_all_files_paths(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    info!("Getting all files paths");
-    let mut files = Vec::new();
-    for entry in WalkDir::new(path)
-        .into_iter()
-        .filter_map(|e: Result<walkdir::DirEntry, walkdir::Error>| e.ok())
-    {
-        if entry.metadata()?.is_file() {
-            files.push(entry.path().display().to_string());
-        }
-    }
-    info!("Finished getting all files paths");
-    Ok(files)
-}
-
-fn get_all_keys(files: Vec<String>) -> HashSet<String> {
-    let mut file_keys = HashSet::new();
-
-    for file in files {
-        info!("Reading file...");
-        match file::read_file(&file) {
-            Ok(contents) => {
-                let content = utils::remove_using_regex(r"\s+", contents)
-                    .trim()
-                    .to_string();
-                let keys: Vec<String> = utils::get_i18next_keys(content);
-                file_keys.extend(keys);
-            }
-            Err(e) => {
-                error!("Error: {}", e);
-            }
-        }
-    }
-
-    file_keys
-}
-
-fn json_string_from_keys(keys: Vec<String>) -> String {
-    info!("Creating json string from keys");
-    fn insert_key(map: &mut serde_json::Map<String, serde_json::Value>, key_parts: &[&str]) {
-        match key_parts {
-            [head, tail @ ..] if !tail.is_empty() => {
-                let entry = map
-                    .entry((*head).to_string())
-                    .or_insert_with(|| serde_json::json!({}));
-                if let serde_json::Value::Object(ref mut sub_map) = entry {
-                    insert_key(sub_map, tail);
-                }
-            }
-            [head] => {
-                map.insert(
-                    (*head).to_string(),
-                    serde_json::Value::String(String::new()),
-                );
-            }
-            _ => {}
-        }
-    }
-
-    let mut map = serde_json::Map::new();
-    for key in keys {
-        let key_parts: Vec<&str> = key.split('.').collect();
-        insert_key(&mut map, &key_parts);
-    }
-    info!("Finished creating json string from keys");
-    serde_json::to_string_pretty(&serde_json::Value::Object(map)).unwrap()
-}
+use log::{self, warn};
+use utils::{get_all_keys, json_string_from_keys};
 
 fn main() {
     let matches = Command::new("i18next translation file handler")
@@ -83,7 +13,7 @@ fn main() {
 
     match matches.subcommand() {
         Some(("create", _sub_m)) => {
-            let files: Vec<String> = get_all_files_paths("./src/input").unwrap();
+            let files: Vec<String> = utils::get_all_files_paths("./src/input").unwrap();
             let file_keys = get_all_keys(files);
             let file_keys: Vec<String> = file_keys.into_iter().collect();
             let json = json_string_from_keys(file_keys);
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,12 @@
-use log::{self, info};
+use log::{self, error, info};
 use regex::Regex;
+use std::{collections::HashSet, error::Error};
 
-pub fn remove_using_regex(re: &str, s: String) -> String {
+use walkdir::WalkDir;
+
+use crate::file::read_file;
+
+fn remove_using_regex(re: &str, s: String) -> String {
     let re = Regex::new(re).unwrap();
     return re.replace_all(&s, "").to_string();
 }
@@ -21,7 +26,7 @@ fn get_prefix(s: &str) -> String {
     result.trim_end().to_string() // Remove trailing space and convert to String
 }
 
-pub fn get_i18next_keys(s: String) -> Vec<String> {
+fn get_i18next_keys(s: String) -> Vec<String> {
     info!("Getting keys from a file");
     let re_keys = Regex::new(r"t\('([^']*)'\)").unwrap();
     let mut processed_keys = Vec::new();
@@ -30,7 +35,6 @@ pub fn get_i18next_keys(s: String) -> Vec<String> {
     re_keys.captures_iter(&s).for_each(|keys| {
         keys.iter().for_each(|k| {
             let text = remove_using_regex(&re_keys.to_string(), k.unwrap().as_str().to_string());
-            println!("Key: {}", text);
             processed_keys.push(text);
         });
     });
@@ -38,4 +42,70 @@ pub fn get_i18next_keys(s: String) -> Vec<String> {
     let filtered_keys = key_list.filter(|k| k != "").map(|k| format!("{prefix}{k}"));
     info!("Finished getting keys from a file");
     filtered_keys.into_iter().collect()
+}
+
+pub fn get_all_files_paths(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    info!("Getting all files paths");
+    let mut files = Vec::new();
+    for entry in WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e: Result<walkdir::DirEntry, walkdir::Error>| e.ok())
+    {
+        if entry.metadata()?.is_file() {
+            files.push(entry.path().display().to_string());
+        }
+    }
+    info!("Finished getting all files paths");
+    Ok(files)
+}
+
+pub fn get_all_keys(files: Vec<String>) -> HashSet<String> {
+    let mut file_keys = HashSet::new();
+
+    for file in files {
+        info!("Reading file...");
+        match read_file(&file) {
+            Ok(contents) => {
+                let content = remove_using_regex(r"\s+", contents).trim().to_string();
+                let keys: Vec<String> = get_i18next_keys(content);
+                file_keys.extend(keys);
+            }
+            Err(e) => {
+                error!("Error: {}", e);
+            }
+        }
+    }
+
+    file_keys
+}
+
+pub fn json_string_from_keys(keys: Vec<String>) -> String {
+    info!("Creating json string from keys");
+    fn insert_key(map: &mut serde_json::Map<String, serde_json::Value>, key_parts: &[&str]) {
+        match key_parts {
+            [head, tail @ ..] if !tail.is_empty() => {
+                let entry = map
+                    .entry((*head).to_string())
+                    .or_insert_with(|| serde_json::json!({}));
+                if let serde_json::Value::Object(ref mut sub_map) = entry {
+                    insert_key(sub_map, tail);
+                }
+            }
+            [head] => {
+                map.insert(
+                    (*head).to_string(),
+                    serde_json::Value::String(String::new()),
+                );
+            }
+            _ => {}
+        }
+    }
+
+    let mut map = serde_json::Map::new();
+    for key in keys {
+        let key_parts: Vec<&str> = key.split('.').collect();
+        insert_key(&mut map, &key_parts);
+    }
+    info!("Finished creating json string from keys");
+    serde_json::to_string_pretty(&serde_json::Value::Object(map)).unwrap()
 }
